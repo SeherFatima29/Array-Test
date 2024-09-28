9763ca16d1ef2479b38bb63120d3a8c93bf7b9d2
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-09-28 18:40:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let array=[]
+let name=prompt("enter your name")
+array.push(name)
+console.log(array);
+
+let age=prompt("enter your age")
+array.push(age)
+console.log(array);
+
+let add=prompt("2+2")
+array.push(add)
+console.log(array);
+
+
+let sub=prompt("10-5")
+array.push(sub)
+console.log(array);
